5add218cc84e3ac42125bd51b1b1c5a73bd5693b
pcygan <pcygan@pcygan>
2018-06-13 15:10:32 +0200

    Test compiles - check if there are exactly 3 nodes

--- Test/TestSpatialLevelGenerator.cpp
+++ Test/TestSpatialLevelGenerator.cpp
@@ -1,7 +1,10 @@
+#include <vector>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/properties.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/range/iterator_range_core.hpp>
 
 using namespace testing;
 
@@ -10,45 +13,50 @@ struct TestSpatialLevelGenerator : public Test
 
 };
 
-struct area_coords_t {
-    typedef boost::vertex_property_tag kind;
-};
-struct area_coords {
-    int top;
-    int left;
-    int bottom;
-    int right;
-};
-using AreaCoordsProperty = boost::property<area_coords_t, area_coords>;
-using SpacePartitioningGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, AreaCoordsProperty>;
-
-void divide(auto& areaCoordsMap, auto& g, auto& vertex, int min_width, int min_height)
+class SpacePartition 
 {
-    auto areaCoords = boost::get(areaCoordsMap, vertex, g);
-    auto width = areaCoords.right - areaCoords.left;
-    auto height = areaCoords.bottom - areaCoords.bottom;
-    if (width >= min_width and height >= min_height)
+public:
+    struct area_coords_t { using kind = boost::vertex_property_tag; };
+    struct area_coords { int top, bottom, left, right; };
+    using AreaCoordsProperty = boost::property<area_coords_t, area_coords>;
+    using SpacePartitioningGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, AreaCoordsProperty>;
+    using Node = boost::graph_traits<SpacePartitioningGraph>::vertex_descriptor;
+
+    SpacePartition(int min_width, int min_height)
+    : min_width(min_width)
+    , min_height(min_height)
+    {}
+
+    void divide(int space_wdth, int space_height)
     {
 
     }
-}
 
-auto add_vertex(auto& areaCoordsMap, auto& g, auto& areaCoords)
-{
-    auto vertex = boost::add_vertex(g);
-    boost::put(areaCoordsMap, vertex, area_coords{0, 0, 20, 20});
-    return vertex;
-}
+    auto nodes()
+    {
+        auto v_it = boost::vertices(graph);
+        return std::vector<Node>(v_it.first, v_it.second);
+    }
+
+private:
+    void add_child(auto parent, auto child)
+    {
+        boost::add_edge(std::make_pair(parent, child), graph);
+    }
+
+    SpacePartitioningGraph graph;
+    boost::property_map<SpacePartitioningGraph, area_coords_t>::type areaCoordMap;
+    const int min_height;
+    const int min_width;
+};
 
 TEST_F(TestSpatialLevelGenerator, envTest)
 {
-    SpacePartitioningGraph g;
-    auto areaCoordsMap = boost::get(area_coords_t(), g);
     const auto min_height = 10;
     const auto min_width = 10;
-    divide(areaCoordsMap, g,
-        add_vertex(areaCoordsMap, g, area_coords{0, 0, 20, 20}),
-        min_height,
-        min_width);
-
+    SpacePartition sp(min_width, min_height);
+    const auto height = 20;
+    const auto width = 20;
+    sp.divide(width, height);
+    ASSERT_EQ(3, sp.nodes().size());
 }
