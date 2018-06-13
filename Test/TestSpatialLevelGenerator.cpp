a39f17a5cb83ed86268e4e484e1f3892046f5e7b
pcygan <pcygan@pcygan>
2018-06-13 15:35:44 +0200

    divide impl ongoing, code compiles but files

--- Test/TestSpatialLevelGenerator.cpp
+++ Test/TestSpatialLevelGenerator.cpp
@@ -22,14 +22,14 @@ public:
     using SpacePartitioningGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, AreaCoordsProperty>;
     using Node = boost::graph_traits<SpacePartitioningGraph>::vertex_descriptor;
 
-    SpacePartition(int min_width, int min_height)
-    : min_width(min_width)
-    , min_height(min_height)
-    {}
-
-    void divide(int space_wdth, int space_height)
+    bool divide(int min_room_width, int min_room_height, int space_width, int space_height)
     {
-
+        min_width = min_room_width;
+        min_height = min_room_height;
+        graph = SpacePartitioningGraph();
+        areaCoordsMap = boost::get(area_coords_t(), graph);
+        auto root = add_vertex(graph);
+        boost::put(areaCoordsMap, root, area_coords{0, space_height, 0, space_width});
     }
 
     auto nodes()
@@ -39,24 +39,33 @@ public:
     }
 
 private:
+    void divide(auto node)
+    {
+        const auto areaCoord = boost::get(areaCoordsMap, node);
+        const auto width = areaCoord.right - areaCoord.left;
+        const auto height = areaCoord.bottom - areaCoord.top;
+        const auto random_min_width = min_width;
+        const auto random_max_width = width - min_width;
+    }
+
     void add_child(auto parent, auto child)
     {
         boost::add_edge(std::make_pair(parent, child), graph);
     }
 
     SpacePartitioningGraph graph;
-    boost::property_map<SpacePartitioningGraph, area_coords_t>::type areaCoordMap;
-    const int min_height;
-    const int min_width;
+    boost::property_map<SpacePartitioningGraph, area_coords_t>::type areaCoordsMap;
+    int min_height;
+    int min_width;
 };
 
 TEST_F(TestSpatialLevelGenerator, envTest)
 {
+    SpacePartition sp;
     const auto min_height = 10;
     const auto min_width = 10;
-    SpacePartition sp(min_width, min_height);
     const auto height = 20;
     const auto width = 20;
-    sp.divide(width, height);
+    ASSERT_TRUE(sp.divide(min_width, min_height, width, height));
     ASSERT_EQ(3, sp.nodes().size());
 }
