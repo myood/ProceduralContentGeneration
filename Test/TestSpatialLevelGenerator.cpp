ddfe7be2fd218a3602b443692957be0e39e7e47d
pcygan <pcygan@pcygan>
2018-06-13 17:58:29 +0200

    Code compiles, test crashes on Illegal instruction

--- Test/TestSpatialLevelGenerator.cpp
+++ Test/TestSpatialLevelGenerator.cpp
@@ -23,24 +23,24 @@ public:
     : rng(rng)
     {}
 
-    struct area_coords_t { using kind = boost::vertex_property_tag; };
-    struct area_coords { int top, bottom, left, right; };
-    using AreaCoordsProperty = boost::property<area_coords_t, area_coords>;
-    using SpacePartitioningGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, AreaCoordsProperty>;
+    struct area_tag { using kind = boost::vertex_property_tag; };
+    struct area_t { int top, bottom, left, right; };
+    using areasProperty = boost::property<area_tag, area_t>;
+    using SpacePartitioningGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, areasProperty>;
     using Node = boost::graph_traits<SpacePartitioningGraph>::vertex_descriptor;
 
     bool divide(int min_room_width, int min_room_height, int space_width, int space_height)
     {
-        if (min_room_width > floor(space_width / 2) or min_room_height > floor(space_height / 2.0))
+        if (not continueDivision(min_room_width, min_room_height, space_width, space_height))
         {
             return false;
         }
         min_width = min_room_width;
         min_height = min_room_height;
         graph = SpacePartitioningGraph();
-        areaCoordsMap = boost::get(area_coords_t(), graph);
+        areasMap = boost::get(area_tag(), graph);
         auto root = add_vertex(graph);
-        boost::put(areaCoordsMap, root, area_coords{0, space_height, 0, space_width});
+        boost::put(areasMap, root, area_t{0, space_height, 0, space_width});
         divide(root);
     }
 
@@ -51,23 +51,58 @@ public:
     }
 
 private:
-    void divide(auto node)
+    bool continueDivision(int min_room_width, int min_room_height, int space_width, int space_height)
     {
-        const auto areaCoord = boost::get(areaCoordsMap, node);
-        const auto width = areaCoord.right - areaCoord.left;
-        const auto height = areaCoord.bottom - areaCoord.top;
-        const auto random_min_width = min_width;
-        const auto random_max_width = width - min_width;
+        return 
+            min_room_width <= floor(space_width / 2) 
+            and min_room_height <= floor(space_height / 2.0);
     }
 
-    void add_child(auto parent, auto child)
+    void divide(const Node& node)
     {
-        boost::add_edge(std::make_pair(parent, child), graph);
+        const auto area = boost::get(areasMap, node);
+        const auto width = area.right - area.left;
+        const auto height = area.bottom - area.top;
+        
+        if (not continueDivision(min_width, min_height, width, height))
+        {
+            return;
+        }
+
+        auto split_width = width;
+        auto split_height = height;
+        int split_orientation = rng(0, 1);
+        if (split_orientation == 0)
+        {
+            const auto random_min_width = min_width;
+            const auto random_max_width = width - min_width;
+            split_width = rng(random_min_width, random_max_width);
+        }
+        else
+        {
+            const auto random_min_height = min_height;
+            const auto random_max_height = height - min_height;
+            split_height = rng(random_min_height, random_max_height);
+        }
+
+
+        divide(
+            add_child(node, area_t{area.top, area.top + split_height, area.left, area.left + split_width}));
+        divide(
+            add_child(node, area_t{area.top + split_height, area.bottom, area.left + split_width, area.bottom}));
+    }
+
+    Node add_child(const Node& parent, const area_t& area)
+    {
+        auto child = add_vertex(graph);
+        boost::put(areasMap, child, area);
+        boost::add_edge(parent, child, graph);
+        return child;
     }
 
     RandomNumberGenerator rng;
     SpacePartitioningGraph graph;
-    boost::property_map<SpacePartitioningGraph, area_coords_t>::type areaCoordsMap;
+    boost::property_map<SpacePartitioningGraph, area_tag>::type areasMap;
     int min_height;
     int min_width;
 };
@@ -91,6 +126,8 @@ TEST_F(TestSpatialLevelGenerator, doesNotAcceptMinDimensionGreaterThenHalfOfThat
 TEST_F(TestSpatialLevelGenerator, envTest)
 {
     RngMock rngMock;
+    EXPECT_CALL(rngMock, generate(0, 1)).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(rngMock, generate(10/*min*/, 10/*max*/)).WillRepeatedly(testing::Return(10));
     SpacePartition sp{rngMock.getFunction()};
     const auto min_height = 10;
     const auto min_width = 10;
