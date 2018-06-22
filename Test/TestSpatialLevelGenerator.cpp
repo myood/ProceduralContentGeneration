5b63bdc361ea6602bdda8df5babe481449ab49c6
pcygan <pcygan@pcygan>
2018-06-22 14:09:45 +0200

    Add new test for grid recreation

--- Test/TestSpatialLevelGenerator.cpp
+++ Test/TestSpatialLevelGenerator.cpp
@@ -35,51 +35,72 @@ bool operator==(const Grid& lhs, const Grid& rhs)
     return os;
 }
 
-TEST(TestSpatialLevelGenerator, shouldRecreateSize)
-{
-    auto createGrid = [](int width, int height, std::vector<SpacePartition::area_t> areas)
-    { 
-        auto grid = std::vector<std::vector<TILE>>(width, std::vector<TILE>(height, TILE::FLOOR));
-        for (const auto& a : areas)
+auto createGrid = [](int width, int height, std::vector<SpacePartition::area_t> areas)
+{ 
+    auto grid = std::vector<std::vector<TILE>>(width, std::vector<TILE>(height, TILE::FLOOR));
+    for (const auto& a : areas)
+    {
+        //top 5, bottom 10, left 3, right 7
+        for (int i = a.top; i <= a.bottom; ++i)
         {
-            //top 5, bottom 10, left 3, right 7
-            for (int i = a.top; i <= a.bottom; ++i)
-            {
-                grid[i][a.left] = TILE::WALL_SINGLE;
-                grid[i][a.right] = TILE::WALL_SINGLE;
-            }
-
-            for (int j = a.left; j <= a.right; ++j)
-            {
-                grid[a.top][j] = TILE::WALL_SINGLE;
-                grid[a.bottom][j] = TILE::WALL_SINGLE;
-            }
+            grid[i][a.left] = TILE::WALL_SINGLE;
+            grid[i][a.right] = TILE::WALL_SINGLE;
         }
-        return grid;
-    };
+
+        for (int j = a.left; j <= a.right; ++j)
+        {
+            grid[a.top][j] = TILE::WALL_SINGLE;
+            grid[a.bottom][j] = TILE::WALL_SINGLE;
+        }
+    }
+    return grid;
+};
+
+TEST(TestSpatialLevelGenerator, rooms4squareSize10x10)
+{
     const auto areas = std::vector<SpacePartition::area_t>{
-        SpacePartition::area_t{ 0, 0, 9, 9 },
-        SpacePartition::area_t{ 0, 10, 9, 19 },
-        SpacePartition::area_t{ 10, 0, 19, 9 },
-        SpacePartition::area_t{ 10, 10, 19, 19 },
+        SpacePartition::area_t{ 0, 0, 4, 4 },
+        SpacePartition::area_t{ 0, 5, 4, 9 },
+        SpacePartition::area_t{ 5, 0, 9, 4 },
+        SpacePartition::area_t{ 5, 5, 9, 9 },
     };
-    const auto width = 20;
-    const auto height = 20;
+    const auto width = 10;
+    const auto height = 10;
     const auto W = TILE::WALL_SINGLE;
     const auto F = TILE::FLOOR;
-    auto expectedGrid = std::vector<std::vector<TILE>>(20, std::vector<TILE>(20, F));
-    for (int i = 0; i < expectedGrid.size(); ++i)
-    {
-        expectedGrid[i][0] = W;
-        expectedGrid[i][expectedGrid.size() - 1] = W;
-        expectedGrid[i][9] = W;
-        expectedGrid[i][10] = W;
+    auto expectedGrid = std::vector<std::vector<TILE>>{
+        { W, W, W, W, W, W, W, W, W, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, W, W, W, W, W, W, W, W, W },
+        { W, W, W, W, W, W, W, W, W, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, W, W, W, W, W, W, W, W, W }
+    };
 
-        expectedGrid[0][i] = W;
-        expectedGrid[expectedGrid.size() - 1][i] = W;
-        expectedGrid[9][i] = W;
-        expectedGrid[10][i] = W;
-    }
+    ASSERT_EQ(Grid{expectedGrid}, Grid{createGrid(width, height, areas)});
+}
+
+TEST(TestSpatialLevelGenerator, rooms2squareSize10x5)
+{
+    const auto areas = std::vector<SpacePartition::area_t>{
+        SpacePartition::area_t{ 0, 0, 4, 4 },
+        SpacePartition::area_t{ 0, 5, 4, 9 }
+    };
+    const auto width = 10;
+    const auto height = 5;
+    const auto W = TILE::WALL_SINGLE;
+    const auto F = TILE::FLOOR;
+    auto expectedGrid = std::vector<std::vector<TILE>>{
+        { W, W, W, W, W, W, W, W, W, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, F, F, F, W, W, F, F, F, W },
+        { W, W, W, W, W, W, W, W, W, W }
+    };
 
     ASSERT_EQ(Grid{expectedGrid}, Grid{createGrid(width, height, areas)});
 }
